7d5ad8336cba68525bf3906e4434fbaf629e1480
Rujira Kongsomran <biwberry@outlook.com>
2020-04-17 18:41:55 +0700

    finish

--- app/src/main/java/com/rujirakongsomran/foregroundservice/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/foregroundservice/MainActivity.java
@@ -1,14 +1,39 @@
 package com.rujirakongsomran.foregroundservice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText etInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initInstances();
+    }
+
+    private void initInstances() {
+        etInput = (EditText) findViewById(R.id.etInput);
+    }
+
+    public void startService(View v) {
+        String input = etInput.getText().toString();
+
+        Intent serviceIntent = new Intent(this, ExampleService.class);
+        serviceIntent.putExtra("inputExtra", input);
+
+        ContextCompat.startForegroundService(this, serviceIntent);
+    }
+
+    public void stopService(View v) {
+        Intent serviceIntent = new Intent(this, ExampleService.class);
+        stopService(serviceIntent);
     }
 }
